ef434cc2f1be4101412c74fe474ca08fab4aff97
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-01-12 16:49:13 -0800

    updated assertArraysEqual, assertEqual, eqArrays with minor updates

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,14 @@
+// assertArraysEqual
 const assertArraysEqual = function(array1, array2) {
 let arraystring1 = '';
 let arraystring2 = '';
 
   for (let i = 0; i < array1.length; i++) {
     arraystring1 += array1[i];
-  
 }
-
 for (let i = 0; i < array2.length; i++) {
   arraystring2 += array2[i];
-  
 }
-  
   if (arraystring1 === arraystring2) {
   console.log(`✔✔✔ Assertion Passed: ' ${array1}  === ${array2}`);
 } else console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
